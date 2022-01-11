c704ed9694cd0a51157875756a5c91ac76110664
lcb42 <blatherwick.lucy@gmail.com>
2022-01-11 18:36:59 +0000

    Figured out centreing css for home page

--- bag-of-trigs-ui/src/App.tsx
+++ bag-of-trigs-ui/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Home from "./Home"
 
 function App() {
   return (
     <Router>
       <div>
-        <h1>Oops</h1>
         <Routes>
           <Route path="/" element={<Home />}/>
         </Routes>
--- bag-of-trigs-ui/src/Home.tsx
+++ bag-of-trigs-ui/src/Home.tsx
@@ -1,5 +1,12 @@
+import './Home.css';
+
 export default function Home() {
   return (
-    <p>Doop</p>
+    <div className='d-flex justify-contents-center home-content-container'>
+            <div className="home-content">
+                <h1>This is centred (title)</h1>
+                <p>This is centred (sub message)</p>
+            </div>
+    </div>
   );
 }
