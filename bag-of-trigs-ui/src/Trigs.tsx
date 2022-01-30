49ebeb19bc4ee2b030351672881452b69296c049
lcb42 <blatherwick.lucy@gmail.com>
2022-01-30 23:09:16 +0000

    Added second page with placeholder trig cards

--- bag-of-trigs-ui/src/Trigs.tsx
+++ bag-of-trigs-ui/src/Trigs.tsx
@@ -2,6 +2,7 @@ import './Trigs.css';
 
 import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
 
 export default function Trigs() {
     return (
@@ -11,12 +12,60 @@ export default function Trigs() {
                     <Navbar.Brand href="#home">Bag of Trigs</Navbar.Brand>
                 </Container>
             </Navbar>
-            <div className='d-flex flex-column justify-contents-center home-content-container'>
-                <div className="home-content">
-                    <h1>Now to populate this page with lots of trig fact tiles</h1>
+            <div className='d-flex justify-content-center trig-tile-container'>
+                <div className="trig-tile">
+                    <Card border="dark" bg="transparent" style={{ width: '18rem'}}>
+                        <Card.Body>
+                            <Card.Title>Trig Title</Card.Title>
+                            <Card.Text>
+                                <div>
+                                    <p>Fact 1 about trig</p>
+                                    <p>Fact 2 about trig</p>
+                                    <p>Fact 3 about trig</p>
+                                    <p>etc...</p>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="trig-tile">
+                    <Card border="dark" bg="transparent" style={{ width: '18rem'}}>
+                        <Card.Body>
+                            <Card.Title>Trig Title 2</Card.Title>
+                            <Card.Text>
+                                <div>
+                                    <p>Fact 1 about trig</p>
+                                    <p>Fact 2 about trig</p>
+                                    <p>Fact 3 about trig</p>
+                                    <p>etc...</p>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="trig-tile">
+                    <Card border="dark" bg="transparent" style={{ width: '18rem'}}>
+                        <Card.Body>
+                            <Card.Title>Trig Title 3</Card.Title>
+                            <Card.Text>
+                                <div>
+                                    <p>Fact 1 about trig</p>
+                                    <p>Fact 2 about trig</p>
+                                    <p>Fact 3 about trig</p>
+                                    <p>etc...</p>
+                                </div>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
             <Navbar bg="transparent" expand="lg" fixed="bottom"><p className='footer-content'>lucyblatherwick - 2022</p></Navbar>
         </div>
     );
 }
+
+// A: Build example cards //Done//
+// B: Pipe data from file into backend
+// C: Pipe data from backend into frontend
+// D: Map?
+// E: Filters?
