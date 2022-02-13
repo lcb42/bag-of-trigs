52a95ac3859fa1e6b8f506ab61bde648ecfd5ab3
lcb42 <blatherwick.lucy@gmail.com>
2022-02-13 17:46:22 +0000

    Added trig csv parsing to backend

--- server.js
+++ server.js
@@ -2,6 +2,54 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const fs = require("fs");
+const csv = require("csv");
+const { stringify } = require('querystring');
+
+
+// ENABLING FUNCTIONS //
+
+const getTrigs = () => {
+
+    // open and real file contents
+    var data = fs.readFileSync(CSV_FILEPATH, 'utf8');
+
+
+    return convertCSVtoJSON(data)
+}
+
+const convertCSVtoJSON = data => {
+    var lines = data.split("\n");
+
+    var result = [];
+
+    var headers = lines[0].toLowerCase().replaceAll(" ", "_").split(",");
+
+    for(var i=1; i<lines.length;i++){
+
+        var obj = {};
+        var currentline = lines[i].split(",");
+
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+
+        result.push(obj);
+    }
+
+    return result;
+}
+
+
+// SERVE ROUTES
+
+// trigs csv filepath
+const CSV_FILEPATH = "CompleteTrigArchive.csv"
+
+// import trigs from file into object
+const TRIGS = getTrigs();
+
+
 //console.log that server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -9,3 +57,9 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT'});
 })
+
+// a route to request all the trigs wiht default sorting
+/*app.get('', (req, res) => {
+    res.send({ trigs: trigs });
+})*/
+
