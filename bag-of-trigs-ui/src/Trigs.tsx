3c93a8406bbe2c48787eb2fcb4a3c912ee3918c2
lcb42 <blatherwick.lucy@gmail.com>
2023-08-20 12:48:39 +0100

    BoT-2: Moved page surroundings to app file

--- bag-of-trigs-ui/src/Trigs.tsx
+++ bag-of-trigs-ui/src/Trigs.tsx
@@ -2,33 +2,26 @@ import { useEffect, useState } from 'react';
 import './Trigs.css';
 import axios from 'axios';
 
-import NavBar from './components/NavBar';
-import Navbar from "react-bootstrap/Navbar";
-
 import TrigTile from "./components/TrigTile";
 
 export default function Trigs() {
-    //const [data, setData] = useState({trigs: []});
+  //const [data, setData] = useState({trigs: []});
 
-    useEffect(() => {
-        const getTrigs = async () => {
-            const result = await axios('/trigreq');
-            console.log(result);
-        };
-        getTrigs();
-    }, []);
+  useEffect(() => {
+    const getTrigs = async () => {
+      const result = await axios('/trigreq');
+      console.log(result);
+    };
+    getTrigs();
+  }, []);
 
-    return (
-        <div className='page-wrap'>
-            <NavBar/>
-            <div className='d-flex justify-content-center trig-tile-container'>
-                <TrigTile title="Trig Title 1" trigFacts={[]} trigImgPath="/images/trigPic1.jpg"/>
-                <TrigTile title="Trig Title 2" trigFacts={[]}/>
-                <TrigTile title="Trig Title 3" trigFacts={[]}/>
-            </div>
-            <Navbar bg="transparent" expand="lg" fixed="bottom"><p className='footer-content'>lucyblatherwick - 2022</p></Navbar>
-        </div>
-    );
+  return (
+    <div className='d-flex justify-content-center trig-tile-container'>
+      <TrigTile title="Trig Title 1" trigFacts={[]} trigImgPath="/images/trigPic1.jpg"/>
+      <TrigTile title="Trig Title 2" trigFacts={[]}/>
+      <TrigTile title="Trig Title 3" trigFacts={[]}/>
+    </div>
+  );
 }
 
 // A: Build example cards //Done//
