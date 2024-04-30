3115d100e87d60167b2658421c84deaaf73411a9
lcb42 <lucy_blatherwick@autovia.co.uk>
2024-04-30 21:01:06 +0100

    BoT-3/4 Added scss, added breakpoints

--- bag-of-trigs-ui/src/Home.tsx
+++ bag-of-trigs-ui/src/Home.tsx
@@ -1,11 +1,11 @@
-import './Home.css';
+import './Home.scss';
 
 export default function Home() {
   return (
     <div className='d-flex flex-column justify-contents-center home-content-container'>
       <div className="home-content">
-        <h1>Welcome to Bag of Trigs!</h1>
-        <h2>The best place to find your next trig point adventure.</h2>
+        <h1>Welcome to Bag of Trigs</h1>
+        <h2>Research your next trig point adventure!</h2>
         <br/>
         <a href="/trigs" className='btn btn-outline-dark'>Find a trig point!</a>
       </div>
